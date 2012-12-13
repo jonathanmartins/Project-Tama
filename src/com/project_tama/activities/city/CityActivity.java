ac5a2279788f5f777457a3eed4c62df02b009031
Jonathan Martins <jonhmbc@gmail.com>
2012-12-13 02:54:48 -0200

    Adicionado option menu ao jogo

--- src/com/project_tama/activities/city/CityActivity.java
+++ src/com/project_tama/activities/city/CityActivity.java
@@ -5,7 +5,11 @@ import java.util.List;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.project_tama.R;
 import com.project_tama.activities.AbstractActivity;
 import com.project_tama.database.DatabaseHandler;
 import com.project_tama.tamamon.Tamamon;
@@ -56,5 +60,27 @@ public class CityActivity extends AbstractActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.game_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.new_battle:
+			newBattle();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void newBattle() {
+		
 	}
 }
