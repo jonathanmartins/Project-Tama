865c2cc7dc097ed020a03f4de8a5747c4c997d59
Jonathan Martins <jonhmbc@gmail.com>
2012-12-13 01:10:14 -0200

    Salvando tamamon recuperado

--- src/com/project_tama/services/server/GetTamamon.java
+++ src/com/project_tama/services/server/GetTamamon.java
@@ -1,20 +1,25 @@
 package com.project_tama.services.server;
 
 import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.project_tama.activities.city.CityActivity;
 import com.project_tama.activities.view.AccountActivity;
+import com.project_tama.database.DatabaseHandler;
 import com.project_tama.services.HttpClientSingleton;
+import com.project_tama.tamamon.Tamamon;
 
 public class GetTamamon extends AsyncTask<String, String, JSONObject>{
 	
@@ -38,8 +43,8 @@ public class GetTamamon extends AsyncTask<String, String, JSONObject>{
 	        response.getEntity().writeTo(out);
 	        out.close();
 	        JSONObject json = new JSONObject(out.toString());
-	        throw new Exception(json.toString());
-			//return json;
+	        //throw new Exception(json.toString());
+			return json;
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -58,11 +63,32 @@ public class GetTamamon extends AsyncTask<String, String, JSONObject>{
 		SharedPreferences login = activity.getSharedPreferences("login_prefs", AccountActivity.MODE_PRIVATE);
 		
 		if (result.equals("0")) {
-			Toast.makeText(activity, "Não foi possível recuperar o seu tamamon!", Toast.LENGTH_LONG).show();
+			Toast.makeText(activity, "Nï¿½o foi possï¿½vel recuperar o seu tamamon!", Toast.LENGTH_LONG).show();
 			login.edit().putBoolean("ready", false).commit();
 		} else {
 			Toast.makeText(activity, "Tamamon recuperado, divirta-se!", Toast.LENGTH_LONG).show();
 			login.edit().putBoolean("ready", true).commit();
+			
+			DatabaseHandler db = new DatabaseHandler(activity);
+			try {
+				Tamamon tama = db.getTamamon(((Integer) result.get("id")).intValue());
+				tama.setEnergy(((Integer) result.get("energy")).intValue());
+				tama.setLife(((Integer) result.get("life")).intValue());
+				db.updateTamamon(tama);
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			// Reading all tamamons
+	        Log.d("Reading: ", "Reading all contacts..");
+	        List<Tamamon> tamas = db.getAllTamamons();       
+	 
+	        for (Tamamon tm : tamas) {
+	            String log = "Id: "+tm.getId()+", Name: " + tm.getName() + ", Life: " + tm.getLife();
+	            // Writing Tamamons to log
+	            Log.d("Name: ", log);
+	        }
 		}
 		
 		progressDialog.dismiss();
